092a2eca64847a093e0e3b9fd5e1d40e5c97a7e2
ismail chebaane <105134011+ismailchebaane@users.noreply.github.com>
2024-02-02 16:30:45 +0100

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const bodyParser = require("body-parser");
 
 app.use(
     cors({
-        origin: "http://localhost:3000",
+        origin: "https://reviewhub-app.netlify.app/",
         methods: "GET,POST,PUT,DELETE",
         credentials: true,
     })
